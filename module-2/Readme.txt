16a1412889f5f62c7da5ecb38719759ef113abb0
ppl <ppl1121@hotmail.com>
2017-06-30 18:13:24 +0800

    module2

---
+++ module-2/Readme.txt
@@ -0,0 +1,10 @@
+This module-2 assignment is finished by YX.
+
+Others' assignments I reviewed:  
+
+https://hajaramanitra.github.io/coursera-angularjs/module1-solution/
+
+https://akanshagautam3751.github.io/module1Solution/
+
+https://rgoregit.github.io/angularjs-assignments/
+
---
+++ module-2/site/app.js
@@ -0,0 +1,96 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListService', ShoppingListService);
+
+ToBuyController.$inject = ['ShoppingListService'];
+function ToBuyController(ShoppingListService) {
+  var ToBuy = this;
+
+  ToBuy.items = ShoppingListService.getTobuy_itmes();
+  ToBuy.message = ShoppingListService.getMessage();
+  
+
+  ToBuy.buyItem = function (itemIndex) {
+    ShoppingListService.buyItems(itemIndex);
+
+  }
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListService'];
+function AlreadyBoughtController(ShoppingListService) {
+  var AlreadyBought = this;
+
+  AlreadyBought.items = ShoppingListService.getAlreadybought_itmes();
+  AlreadyBought.message = ShoppingListService.getMessage();
+}
+
+
+function ShoppingListService() {
+  var service = this;
+
+  // List of AlreadyBought items
+  var tobuy_items = [
+    {
+      name: "cookies",
+      quantity: 10
+    },
+    {
+      name: "banana",
+      quantity: 5
+    },
+    {
+      name: "chips",
+      quantity: 8
+    },
+    {
+      name: "pepsi",
+      quantity: 2
+    },
+    {
+      name: "apple",
+      quantity: 6
+    },
+    ];
+
+  // List of AlreadyBought items
+  var alreadybought_items = [];
+
+  var message = {
+     tobuy_message: "",
+     alreadybought_message: "Nothing bought yet"
+  } ;
+
+
+  service.buyItems = function (itemIndex) {
+    var boughtItem = tobuy_items.splice(itemIndex, 1)[0];
+    alreadybought_items.push(boughtItem);
+    if(tobuy_items.length == 0){
+      console.log("empty");
+       message.tobuy_message = "Everything is bought!";
+    }
+    if(alreadybought_items.length > 0){
+      message.alreadybought_message = "";
+    }
+  };
+
+
+  service.getTobuy_itmes = function () {
+    return tobuy_items;
+  };
+
+  service.getAlreadybought_itmes = function () {
+    return alreadybought_items;
+  };
+
+  service.getMessage = function () {
+    return message;
+  };
+
+}
+
+})();
